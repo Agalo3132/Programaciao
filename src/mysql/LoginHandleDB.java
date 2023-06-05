92cec86a59b2f1cd4d7c841eb1f4a845414d0e92
Alvaro <alvarosermar38@gmail.com>
2023-06-05 17:20:14 +0200

    p2

--- src/mysql/LoginHandleDB.java
+++ src/mysql/LoginHandleDB.java
@@ -3,10 +3,7 @@ package mysql;
 import model.Login;
 import util.DatabaseConnection;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -36,10 +33,20 @@ public class LoginHandleDB {
     }
 
     public Login getLogin(String name, String password){
-        try (Connection connection = DatabaseConnection.getConnection()){
 
-
-
+        String query = "SELECT * FROM login WHERE username=" + name + " AND password=" + password;
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement ps = connection.prepareStatement(query)){
+            ResultSet rs = ps.getResultSet();
+            if (rs.next()){
+                Login lg = new Login();
+                lg.setId(rs.getInt(1));
+                lg.setUsername(rs.getString("username"));
+                lg.setPassword(rs.getString("password"));
+                lg.setCreatedAt(rs.getTimestamp("created_at").toLocalDateTime());
+                return lg;
+            }
+            return null;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
